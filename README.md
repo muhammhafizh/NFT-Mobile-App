23df921561774c5f38ff1c8dc276828b655d012f
muhammhafizh <hafizhsusanto9@gmail.com>
2023-01-30 23:40:54 +0700

    NFT Expo

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Native App With Expo
---
+++ components/index.js
@@ -0,0 +1,16 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailDesc from "./DetailDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  RectButton,
+  CircleButton,
+  DetailDesc,
+  DetailsBid,
+};
